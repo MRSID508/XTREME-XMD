40cd8b144e3c8fcb7a21e36be547eeef02fce4dd
MRSID508 <sidmr771@gmail.com>
2025-08-01 19:24:38 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~2oo0nL6I#0Xbvl0_5d6gGW3Z-GKrOb4L_6Bguslzo2oDPtF0O4BQ",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -41,7 +41,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "52 81 4555 0855",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "528145550855",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ᴘʀɪɴᴄᴇ sɪᴅ",
 // add bot owner name
